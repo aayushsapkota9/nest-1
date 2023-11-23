e4ba94dae78c4e7a7d3e02f0a17dfd8e48359a44
aayushsapkota9 <aayushsapkota9@gmail.com>
2023-11-23 14:20:40 +0545

    providers

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,33 +1,19 @@
-import {
-  Body,
-  Controller,
-  Get,
-  Header,
-  HttpCode,
-  Param,
-  Post,
-} from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { CatsService } from './cats.service';
+import { Cat } from 'interfaces/cat.interface';
 import { CreateCatDto } from './create-cat.dto';
 
 @Controller('cats')
 export class CatsController {
-  @Header('Cache-Control', 'none')
+  constructor(private catsService: CatsService) {}
+
   @Post()
-  create(@Body() createCatDto: CreateCatDto): string {
-    return 'This action adds a new cat';
+  async create(@Body() createCatDto: CreateCatDto) {
+    this.catsService.create(createCatDto);
   }
-  @Get('all')
-  @HttpCode(204)
-  findAll(): string {
-    return 'This action returns all cats';
-  }
-  @Get(':id')
-  findOne(@Param() params: any): string {
-    console.log(params.id);
-    return `This action returns a #${params.id} cat`;
-  }
-  @Get('ab*cd')
-  find() {
-    return 'This route uses a wildcard';
+
+  @Get()
+  async findAll(): Promise<Cat[]> {
+    return this.catsService.findAll();
   }
 }
