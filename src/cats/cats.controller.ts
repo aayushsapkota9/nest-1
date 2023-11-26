50e3149a6058013746fd7941014b4788369c8c81
aayushsapkota9 <aayushsapkota9@gmail.com>
2023-11-26 09:36:11 +0545

    exceptionfilters and middlewaers

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,20 +1,32 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  HttpStatus,
+  HttpException,
+  UseFilters,
+  ForbiddenException,
+} from '@nestjs/common';
 import { CatsService } from './cats.service';
 import { Cat } from 'src/cats/interfaces/cat.interface';
 import { CreateCatDto } from './dto/create-cats.dto';
+import { HttpExceptionFilter } from 'src/httpexception/http-exception.filter';
 // import { CreateCatDto } from './create-cat.dto';
 
 @Controller('cats')
+@UseFilters(HttpExceptionFilter)
 export class CatsController {
   constructor(private catsService: CatsService) {}
 
   @Post()
   async create(@Body() createCatDto: CreateCatDto) {
     this.catsService.create(createCatDto);
+    throw new ForbiddenException();
   }
 
   @Get()
-  async findAll(): Promise<Cat[]> {
-    return this.catsService.findAll();
+  async findAll() {
+    throw new HttpException('Not Found', 404);
   }
 }
