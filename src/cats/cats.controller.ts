e8803594e2bf1162a1fa6df68af2448237da108a
aayushsapkota9 <aayushsapkota9@gmail.com>
2023-11-29 09:18:03 +0545

    guards

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -11,16 +11,20 @@ import {
   ParseIntPipe,
   UsePipes,
   ValidationPipe,
+  UseGuards,
 } from '@nestjs/common';
 import { CatsService } from './cats.service';
 import { Cat } from 'src/cats/interfaces/cat.interface';
 import { CreateCatDto } from './dto/create-cats.dto';
 import { HttpExceptionFilter } from 'src/httpexception/http-exception.filter';
+import { RolesGuard } from 'src/auth.guard';
+import { Roles } from 'src/roles.decorator';
 // import { ZodValidationPipe } from './validation.pipe';
 // import { CreateCatDto } from './create-cat.dto';
 
 @Controller('cats')
 @UseFilters(HttpExceptionFilter)
+@UseGuards(RolesGuard)
 export class CatsController {
   constructor(private catsService: CatsService) {}
 
@@ -30,10 +34,10 @@ export class CatsController {
   //   this.catsService.create(createCatDto);
   // }
 
-  @Post()
-  async create(@Body(new ValidationPipe()) createCatDto: CreateCatDto) {
-    this.catsService.create(createCatDto);
-  }
+  // @Post()
+  // async create(@Body(new ValidationPipe()) createCatDto: CreateCatDto) {
+  //   this.catsService.create(createCatDto);
+  // }
 
   @Get()
   async findAll() {
@@ -49,5 +53,11 @@ export class CatsController {
     id: number,
   ) {
     return this.catsService.findOne(id);
+  }
+
+  @Post()
+  @Roles(['admin'])
+  async create(@Body() createCatDto: CreateCatDto) {
+    this.catsService.create(createCatDto);
   }
 }
